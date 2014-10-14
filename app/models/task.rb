5852bed4645cfca1f53353e441830477d5f1da09
Gk <gk@gkparishphilp.com>
2014-10-14 16:40:00 -0700

    - quick pass on tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,12 @@
 class Task < ActiveRecord::Base
 
+	enum status: { 'pending' => 0, 'active' => 1, 'completed' => 2, 'confirmed' => 3, 'trash' => 4 }
+
 	belongs_to	:agency
+
+
+	def self.overdue
+		where( "due_at < :now", now: Time.zone.now )
+	end
 	
 end
