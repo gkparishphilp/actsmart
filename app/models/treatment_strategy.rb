584e04121b1861996a002730f34801f53f20dd89
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-10-19 13:01:05 -0700

    Activity 3.3.1 auto generates Tasks if selected == true

--- app/models/treatment_strategy.rb
+++ app/models/treatment_strategy.rb
@@ -1,4 +1,15 @@
 class TreatmentStrategy < ActiveRecord::Base
 	belongs_to :agency
 
+	after_save :create_task
+
+	private
+
+	def create_task
+		if self.changed? && self.selected == true
+			Task.create name: self.name, content: self.specifics, agency_id: self.agency_id,
+			assigned_to: implementer
+		end
+	end
+
 end
