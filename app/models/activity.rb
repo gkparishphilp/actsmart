47f6e29076715a04145084a6bdb308b9e8d7fb9f
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-10-20 12:08:37 -0700

    Funding, treatment adaptations/strategies export to CSV

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -17,9 +17,39 @@ class Activity < ActiveRecord::Base
 
 	def to_csv(current_agency)
 		CSV.generate do |csv|
-			csv << ['Responses']
-			self.responses.where(agency_id: current_agency.id).order("id ASC").each do |response|
-				csv << [response.content]
+
+			if self.id == 9
+				csv << ['Funding Sources']
+				current_agency.funding_sources.order("id ASC").each do |funding|
+					csv << [funding.name]
+					csv << [funding.offer_funding]
+					csv << [funding.reimbursement_rate]
+				end
+			end
+
+			if self.id == 11 
+				csv << ['Treatment Strategies']
+				current_agency.treatment_strategies.order("id ASC").each do |treatment_strategies|
+					csv << [treatment_strategies.attributes]
+				end	
+
+			elsif self.id == 12
+				csv << ['Prospective Adaptations', 'No Longer Considering Adaptation']
+				current_agency.treatment_adaptations.order("id ASC").each do |treatment_adaptations|
+					csv << [treatment_adaptations.name, treatment_adaptations.not_doing_adaptation]
+				end	
+
+			elsif self.id == 13 
+				csv << ['Treatment Adaptations']
+				current_agency.treatment_adaptations.order("id ASC").each do |treatment_adaptations|
+					csv << [treatment_adaptations.attributes]
+				end
+
+			else
+				csv << ['Responses']
+				self.responses.where(agency_id: current_agency.id).order("id ASC").each do |response|
+					csv << [response.content]
+				end
 			end
 		end
 	end
